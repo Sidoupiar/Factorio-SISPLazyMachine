79ff79fca62142a5dbc6d2623f3e019d09c4f157
Sidoupiar <sidoupiar@qq.com>
2021-03-31 00:35:17 +0800

    调整数值

--- control.lua
+++ control.lua
@@ -10,7 +10,9 @@ load()
 -- ------------------------------------------------------------------------------------------------
 
 SINoise = need( "__SICoreFunctionLibrary__/function/lib/sinoise" )
-LevelCount = ( SISPLM.levelCount + 1 ) / 2
+
+LazyReg = "%-lazy[0-9]+$"
+LevelCount = ( SISPLM.levelCount + 8 ) / 2
 
 -- ------------------------------------------------------------------------------------------------
 -- ---------- 更新机器 ----------------------------------------------------------------------------
@@ -18,22 +20,33 @@ LevelCount = ( SISPLM.levelCount + 1 ) / 2
 
 function Build( event )
 	local entity = event.created_entity or event.entity
-	if entity.type == SITypes.entity.furnace or entity.type == SITypes.entity.machine then
+	local type = entity.type
+	if type == SITypes.entity.furnace or type == SITypes.entity.machine then
+		local surface = entity.surface
+		local pos = entity.position
+		local noise = SINoise.New( surface.map_gen_settings.seed , 4 , 0.7 , 25 )
+		local level = math.floor( (noise:Get( pos.x , pos.y )+1)*LevelCount ) - 7
+		level = math.fmod( level , SISPLM.levelCount+1 )
+		--sip( "pos : {"..pos.x..","..pos.y.."} , source : "..noise:Get( pos.x , pos.y ).." , level : "..level ) -- 输出控制台
+		
+		local flag = true
 		local name = entity.name
-		if not name:find( "-lazy[0-9]+$" ) then
-			local surface = entity.surface
-			local pos = entity.position
-			local noise = SINoise.New( surface.map_gen_settings.seed , 4 , 0.7 , 25 )
-			local level = math.floor( (noise:Get( pos.x , pos.y )+1)*LevelCount )
-			level =  math.fmod( level , SISPLM.levelCount+1 )
-			--sip( "pos : {"..pos.x..","..pos.y.."} , source : "..noise:Get( pos.x , pos.y ).." , level : "..level ) -- 输出控制台
-			if level < 1 then return end
-			name = name .. "-lazy" .. level
-			local new_entity = surface.create_entity{ name = name , position = pos , direction = entity.direction , force = entity.force , raise_built = true }
-			if not new_entity then return end
-			new_entity.copy_settings( entity )
-			entity.destroy{ raise_destroy = true }
+		local ind = name:find( LazyReg )
+		if ind then
+			local oldLevel = tonumber( name:sub( ind+5 ) )
+			if oldLevel == level then return
+			else
+				flag = false
+				name = name:sub( 1 , ind-1 )
+			end
 		end
+		if level < 1 and flag then return end
+		
+		name = name .. "-lazy" .. level
+		local new_entity = surface.create_entity{ name = name , position = pos , direction = entity.direction , force = entity.force , raise_built = true }
+		if not new_entity then return end
+		new_entity.copy_settings( entity )
+		entity.destroy{ raise_destroy = true }
 	end
 end
 
